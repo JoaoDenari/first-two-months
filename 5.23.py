c13df19c318b72b98ad865169ffb296f6091522a
João Vitor Denari dos Santos <98748740+JoaoDenari@users.noreply.github.com>
2022-02-05 16:41:37 -0300

    Update 5.23.py

--- 5.23.py
+++ 5.23.py
@@ -1,10 +1,8 @@
-Python 3.9.9 (tags/v3.9.9:ccb0e6a, Nov 15 2021, 18:08:50) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 0
->>> n = 0
->>> resto = 0
->>> p = 0
->>> while True:
+a = 0
+n = 0
+resto = 0
+p = 0
+while True:
 	a = int(input("Insira o número a ser analisado: "))
 	while a >= n:
 		n += 1
